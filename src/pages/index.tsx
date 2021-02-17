90ae9ad87adff312e45a22ffdf36ee8b37786bc1
Caio Henrique <caio_bnh22@hotmail.com>
2021-02-17 20:18:15 -0300

    meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
       <Header />
