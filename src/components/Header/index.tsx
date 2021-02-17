018c836fde1edeef3cde37366f55acc1c691712a
Caio Henrique <caio_bnh22@hotmail.com>
2021-02-17 20:10:26 -0300

    primeiro layer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import Image from 'next/image';
+import { useMediaQuery } from 'react-responsive';
+
 import styles from './Header.module.css';
 
-const Header: React.FC = () => (
-  <>
-    <div
-      className={styles.container}
-      style={{ backgroundImage: 'url("images/header/Fundo-sem-personagem.png")' }}
-    >
-      <div
-        className={styles.joker}
-        style={{ backgroundImage: 'url("images/header/Personagem.png")' }}
-      />
+const Header: React.FC = () => {
+  const switchBackground = useMediaQuery({ query: '(max-width: 800px)' });
 
+  return (
+    <>
       <div
-        className={styles.tournament}
+        className={styles.container}
+        style={{ backgroundImage: switchBackground ? 'url("images/header/Fundo-sem-personagem.png")' : 'url("images/header/Fundo.png")' }}
       >
-        <Image className={styles.image} src="/images/header/Logotipo.png" width="408" height="405" />
-        <Image className={styles.image} src="/images/header/Botão-roxo.png" width="338" height="97" />
-        <Image className={styles.image} src="/images/header/botão-azul.png" width="200" height="35" />
-        <Image className={styles.image} src="/images/header/botão-rosa.png" width="200" height="35" />
+
+        <div
+          className={styles.tournament}
+        >
+          <div className={styles.logo}>
+            <Image src="/images/header/Logotipo.png" width="408" height="405" />
+          </div>
+          <div className={styles.button}>
+            <Image src="/images/header/Botão-roxo.png" width="338" height="97" />
+          </div>
+          <div className={styles.button}>
+            <Image src="/images/header/botão-azul.png" width="559" height="97" />
+          </div>
+          <div className={styles.button}>
+            <Image src="/images/header/botão-rosa.png" width="595" height="106" />
+          </div>
+
+        </div>
 
       </div>
 
-    </div>
-
-  </>
-);
+    </>
+  );
+};
 
 export default Header;
