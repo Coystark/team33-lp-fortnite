4347d0b10c2588de6f136049ac11c60a4246e421
Caio Henrique <caio_bnh22@hotmail.com>
2021-02-18 11:52:57 -0300

    primeiro layer terminado

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,68 +1,53 @@
 import React from 'react';
 import Image from 'next/image';
-
-import styled from 'styled-components';
-import styles from './Header.module.css';
-
-const JustTesting = styled.div`
-  height: 100vh;
-  width: 100%;
-  background-size: cover;
-  background-image: url("images/header/header-bg-character.jpg");
-
-  @media(max-width: 1049px) {
-    background-image: url("images/header/header-bg-without-character.jpg");
-  }
-`;
-
-const Joker = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-image: url("images/header/Personagem.png");
-
-  @media(max-width: 1049px) {
-    display: none;
-  }
-`;
-
-/*
-  background-image: url("images/header/Fundo.png");
-
-  @media(max-width: 768px) {
-    background-image: url("images/header/Fundo-sem-personagem.png");
-  }
-*/
-// <Joker />
+import {
+  Container,
+  Tournament,
+  TournamentItems,
+  TournamentLogo,
+  TournamentPrizePool,
+  TournamentSignUpNow,
+  TournamentTotalPrizePool,
+} from './styles';
 
 const Header: React.FC = () => (
-  <>
-    <JustTesting>
-
-      <div
-        className={styles.tournament}
-      >
-        <div className={styles.tournament__items}>
-          <div className={styles.logo}>
-            <Image src="/images/header/Logotipo.png" width="408" height="405" />
-          </div>
-          <div className={styles.button}>
-            <Image src="/images/header/Botão-roxo.png" width="338" height="97" />
-          </div>
-          <div className={styles.button}>
-            <Image src="/images/header/botão-azul.png" width="559" height="97" />
-          </div>
-          <div className={styles.button}>
-            <Image src="/images/header/botão-rosa.png" width="595" height="106" />
-          </div>
-        </div>
-      </div>
-
-    </JustTesting>
-
-  </>
+  <Container>
+    <Tournament>
+      <TournamentItems>
+        <TournamentLogo>
+          <Image
+            draggable={false}
+            src="/images/header/tournament-logo.png"
+            width="408"
+            height="405"
+          />
+        </TournamentLogo>
+        <TournamentPrizePool>
+          <Image
+            draggable={false}
+            src="/images/header/prize-pool.png"
+            width="338"
+            height="97"
+          />
+        </TournamentPrizePool>
+        <TournamentTotalPrizePool>
+          <Image
+            draggable={false}
+            src="/images/header/total-prize-pool.png"
+            width="559"
+            height="97"
+          />
+        </TournamentTotalPrizePool>
+        <TournamentSignUpNow href="/#">
+          <Image
+            src="/images/header/sign-up-now.png"
+            width="595"
+            height="106"
+          />
+        </TournamentSignUpNow>
+      </TournamentItems>
+    </Tournament>
+  </Container>
 );
 
 export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-size: cover;
+  background-image: url("images/header/header-bg-character.jpg");
+
+  @media(max-width: 1049px) {
+    background-image: url("images/header/header-bg-without-character.jpg");
+  }
+`;
+
+export const Tournament = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  justify-content: center;
+  margin: 0 20px 0 20px;
+  user-select: none;
+
+  @media only screen and (min-width: 1049px) {
+    margin-right: 200px;
+    padding-top: 120px;
+  }
+
+  @media only screen and (max-width: 1049px) {
+    align-items: center;
+  }
+`;
+
+export const TournamentItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TournamentLogo = styled.div`
+  margin-bottom: 20px;
+`;
+
+export const TournamentPrizePool = styled.div`
+  margin-bottom: 20px;
+
+  @media(max-width: 1049px) {
+    width: 50%;
+    height: auto;
+  }
+
+`;
+
+export const TournamentTotalPrizePool = styled.div`
+  margin-bottom: 20px;
+
+`;
+
+export const TournamentSignUpNow = styled.a`
+  margin-bottom: 20px;
+
+`;
